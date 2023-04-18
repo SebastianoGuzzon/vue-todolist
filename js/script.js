ac15f109387dc5e75627269d73707e026a9a5b45
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-18 18:56:41 +0200

    modifiche js script

--- js/script.js
+++ js/script.js
@@ -1,21 +1,46 @@
-const Vue({
-  el: '#app',
-  data: {
-    newTodo: '',
-    todos: []
+const { createApp } = Vue;
+// Creare uan nuova Task
+createApp({
+  data() {
+    return {
+      newTask: "", 
+      tasks: [], 
+      errorMsg: "", 
+    };
   },
-  
   methods: {
-    addTodo() {
-      if (this.newTodo.trim() !== '') {
-        this.todos.push({ text: this.newTodo });
-        this.newTodo = '';
+    // Metodo per aggiungere un nuovo task
+    addTask() {
+      
+      // Controlla se la lunghezza del task è inferiore a 5 caratteri
+      
+      if (this.newTask.length < 5) {
+        
+        // Oppure messaggio d'errore
+        
+        this.errorMsg = "Il testo deve avere almeno 5 caratteri.";
+      } else {
+        // Aggiunge il nuovo task all'inizio dell'array "tasks"
+        this.tasks.unshift({ text: this.newTask, done: false });
       }
     },
-    
-    removeTodo(todo) {
-      this.todos.splice(this.todos.indexOf(todo), 1);
-    }
-  }
-});
 
+    //metodo per rimuovere un task
+    removeTask (index) {
+    // controlla se un task selezionato e' stato completato  
+    if(this.tasks[index].done){
+    // lo rimuove dall'array tasks
+    this.tasks.splice(index,1);
+    
+    } else {
+    this.errorMsg = "Attenzione puoi eliminare il task solo se e' stato creato";
+    
+    // il messaggio di errore sparisce dopo 3 secondi
+
+    setTimeout (()=>{
+    this.errorMsg = "";  
+    },3000);
+    }
+    }
+  } 
+}).mount("#app");
